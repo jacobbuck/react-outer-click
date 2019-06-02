cf59aa93564c0fd3cabf539d709062fcb613c438
Jacob Buck <buck.jacob@gmail.com>
2019-06-02 17:46:27 +1200

    Update entrypoint file

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  input: 'src/OuterClick.js',
+  input: 'src/index.js',
   output: {
-    file: 'lib/OuterClick.js',
+    file: 'lib/index.js',
     format: 'cjs',
   },
   external: ['prop-types', 'react', 'react-dom'],
