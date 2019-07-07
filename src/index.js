ac3a01a491dc77b897c3991e4108c8113b41d18c
Jacob Buck <buck.jacob@gmail.com>
2019-07-07 12:20:53 +1200

    Add type checking for handler parameter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import PropTypes from 'prop-types';
 import React, { useEffect, useRef } from 'react';
 
 export const useOuterClick = (ref, handler) => {
+  if (process.env.NODE_ENV !== 'production' && typeof handler !== 'function') {
+    throw new TypeError(
+      `Expected "handler" to be a function, not ${typeof handler}.`
+    );
+  }
+
   const handerRef = useRef(handler);
 
   useEffect(() => {
