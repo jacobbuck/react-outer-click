9b7d8d8964efbdbbc98c7280819e8aa28aa69991
Jacob Buck <buck.jacob@gmail.com>
2020-04-27 15:44:15 +1200

    Add esmodule build

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,16 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'lib/index.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: 'lib/index.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'lib/index.esm.js',
+      format: 'esm',
+    },
+  ],
   external: ['prop-types', 'react', 'react-dom'],
   plugins: [babel()],
 };
