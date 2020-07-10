20ad83b80ca383d91a6e736f85df057cabf75206
Jacob Buck <buck.jacob@gmail.com>
2020-07-10 12:29:06 +1200

    Configure rollup and babel to use @babel/runtime

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,14 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/index.js',
   output: [
-    {
-      file: 'lib/index.cjs.js',
-      format: 'cjs',
-    },
-    {
-      file: 'lib/index.esm.js',
-      format: 'esm',
-    },
+    { file: 'lib/index.cjs.js', format: 'cjs' },
+    { file: 'lib/index.esm.js', format: 'esm' },
   ],
-  external: ['prop-types', 'react', 'use-latest'],
-  plugins: [babel()],
+  external: [/@babel\/runtime/, 'prop-types', 'react', 'use-latest'],
+  plugins: [
+    babel({
+      babelHelpers: 'runtime',
+      plugins: ['@babel/plugin-transform-runtime'],
+    }),
+  ],
 };
