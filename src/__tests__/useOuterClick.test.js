c626d6bb9bcc71ed4b0610a08e67098c25e7f63d
Jacob Buck <buck.jacob@gmail.com>
2020-08-29 12:49:18 +1200

    Create useOuterClick.test.js

---
+++ src/__tests__/useOuterClick.test.js
@@ -0,0 +1,53 @@
+import { fireEvent, render } from '@testing-library/react';
+import React, { useRef } from 'react';
+import useOuterClick from '../useOuterClick';
+
+const fireClickEvent = (element) => {
+  fireEvent.mouseDown(element);
+  fireEvent.touchStart(element);
+};
+
+const TestComponent = (props) => {
+  const ref = useRef(null);
+  useOuterClick(ref, props.handler);
+  return (
+    <div data-testid="parent">
+      <div data-testid="sibling">Test</div>
+      <div data-testid="target" ref={ref}>
+        Hello <div data-testid="child">World</div>
+      </div>
+    </div>
+  );
+};
+
+test('throws TypeError when handler is not a function', () => {
+  expect(() => useOuterClick([], { hello: 'world' })).toThrow(
+    new TypeError('Expected "handler" to be a function, not object.')
+  );
+});
+
+test('uses default handler when not set', () => {
+  const { getByTestId } = render(<TestComponent />);
+  expect(() => fireClickEvent(getByTestId('parent'))).not.toThrow();
+});
+
+test('calls handler when element outside ref is clicked/touched', () => {
+  const handler = jest.fn();
+  const { getByTestId } = render(<TestComponent handler={handler} />);
+  [getByTestId('parent'), getByTestId('sibling')].forEach(fireClickEvent);
+  expect(handler).toHaveBeenCalledTimes(4);
+});
+
+test('does not call handler when element ref is clicked/touched', () => {
+  const handler = jest.fn();
+  const { getByTestId } = render(<TestComponent handler={handler} />);
+  fireClickEvent(getByTestId('target'));
+  expect(handler).toHaveBeenCalledTimes(0);
+});
+
+test('does not call handler when element inside ref is clicked/touched', () => {
+  const handler = jest.fn();
+  const { getByTestId } = render(<TestComponent handler={handler} />);
+  fireClickEvent(getByTestId('child'));
+  expect(handler).toHaveBeenCalledTimes(0);
+});
