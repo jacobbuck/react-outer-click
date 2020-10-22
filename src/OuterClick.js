3a8125e7eff5421c62ed986e02e4c454cf76d59f
Jacob Buck <buck.jacob@gmail.com>
2020-10-22 09:02:40 +0000

    Make onOuterClick prop in OuterClick component optional (but preferred)

--- src/OuterClick.js
+++ src/OuterClick.js
@@ -5,7 +5,7 @@ import useComposedRef from 'use-composed-ref';
 import useOuterClick from './useOuterClick';
 
 const OuterClick = React.forwardRef(function OuterClick(
-  { as: Element = 'div', children = null, onOuterClick, ...rest },
+  { as: Element = 'div', children = null, onOuterClick = null, ...rest },
   userRef
 ) {
   const libRef = useRef();
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV !== 'production') {
   OuterClick.propTypes = {
     as: PropTypes.elementType,
     children: PropTypes.node,
-    onOuterClick: PropTypes.func.isRequired,
+    onOuterClick: PropTypes.func,
   };
 }
 
